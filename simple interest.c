ea16494e1d6b4af48e954dcc1a8eda616f73ed0a
shanraoai <shanraopawar7787@gmail.com>
2025-11-17 20:38:11 +0530

    Update simple interest.c

--- simple interest.c
+++ simple interest.c
@@ -7,8 +7,7 @@ int main()
     printf("enter the value of time\n");
     scanf("%d\n",&t);
     printf("enter the value of interset\n");
-    scanf("%d\n",r);
+    scanf("%d\n",&r);
     si=(p*t*r)/100;
     printf("simple interest=%d\n",si);
-    
 }
