e6875da870ab42a24a6fc47e7855dc61d20b12b5
shanraoai <shanraopawar7787@gmail.com>
2025-11-17 16:42:44 +0530

    Create simple interest.c

---
+++ simple interest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int p,t,r,si;
+    printf("enter the value of principle\n");
+    scanf("%d\n",&p);
+    printf("enter the value of time\n");
+    scanf("%d\n",&t);
+    printf("enter the value of interset\n");
+    scanf("%d\n",r);
+    si=(p*t*r)/100;
+    printf("simple interest=%d\n",si);
+    
+}
