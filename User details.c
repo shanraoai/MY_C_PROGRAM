c78f127a572e8174b48f5722e89bb5034931cad1
shanraoai <shanraopawar7787@gmail.com>
2025-11-18 15:56:19 +0530

    Create User details.c

---
+++ User details.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int  jaibalaya(){
+    char name,department;
+    int age;
+    printf("enter a name\n");
+    scanf("%s",&name);
+    printf("enter a age\n");
+    scanf("%d",&age);
+    printf("enter a department\n");
+    scanf("%s",&department);
+}                                                                     int main(){
+     jaibalaya();
+}                                                                 
+
+       
+  
