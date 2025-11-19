1b16e15e7ac44c8674e310796a156bebefcd3140
shanraoai <shanraopawar7787@gmail.com>
2025-11-19 12:07:42 +0530

    Create Largest of three number function.c

---
+++ Largest of three number function.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ int largest(int a,int b,int c){
+        if(a>=b && a>=c){
+            printf("a is largest");
+          }
+         else if(b>=a && b>=c){
+             printf("b is largest");
+          }
+           else {
+               printf("c is largest");
+          }
+    }
+           int main(){
+               largest(4,6,3);{
+          }
+    }
