9a8ad7a4e5cd5dc79104bfe0828028187f77b233
shanraoai <shanraopawar7787@gmail.com>
2025-11-21 10:13:20 +0530

    Update malloc.c

--- malloc.c
+++ malloc.c
@@ -6,4 +6,17 @@
     printf("value stored=%d\n",*p);
     free(p);
     }
-    
+    [or]
+#include<stdio.h>
+#include<stdlib.h>
+ int main(){
+    int *p=(int*)malloc(sizeof(int));
+    if(p==NULL)
+    {
+        printf("memory not allocated\n");
+    }
+    *p=10;
+    printf("value stored=%d\n",*p);
+    free(p);
+ }    
+   
