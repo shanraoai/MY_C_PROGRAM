4f84a6ff28af2d66c9583bda11b04d810afe3b65
shanraoai <shanraopawar7787@gmail.com>
2025-11-21 15:18:41 +0530

    Update Distance between two lines.c

--- Distance between two lines.c
+++ Distance between two lines.c
@@ -5,6 +5,5 @@ int main(){
     printf("enter a values for all\n");
     scanf("%d %d %d %d",&x1,&x2,&y1,&y2);
      d = sqrt(pow((x2 - x1) ,2) + pow((y2 - y1),2));
-
-    printf("distance btween stright lines=%d",d);
+      printf("distance between straight lines=%d",d);
 }
