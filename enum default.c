0f787307fde7529469b6f24b118a48add322500d
shanraoai <shanraopawar7787@gmail.com>
2025-11-22 10:28:44 +0530

    Create enum default.c

---
+++ enum default.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+  enum week {
+      monday, 
+      tuesday, 
+      wednesday, 
+      thursday, 
+      friday, 
+      saturday, 
+      sunday  
+    };
+ int main(){
+  enum week today;
+    today=sunday;
+     printf("%d",today);
+ }     
